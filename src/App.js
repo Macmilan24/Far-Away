8e32e542f89f039488f84cfe79bcf2d3b8ff5dbb
Macmilan24 <cpt.macmilan24@gmail.com>
2024-05-07 14:36:04 +0300

    added finsh message for stat

--- src/App.js
+++ src/App.js
@@ -99,8 +99,10 @@ function Stats({ items }) {
   return (
     <footer className="stats">
       <em>
-        You have {numItems} items on your list, and you already packed{" "}
-        {numPacked} ({percentage}%)
+        {percentage === 100
+          ? " You got everything! Ready to go ✈️🌍"
+          : `You have ${numItems} items on your list, and you already packed
+        ${numPacked} (${percentage}%)`}
       </em>
     </footer>
   );

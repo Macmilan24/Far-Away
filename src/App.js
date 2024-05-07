c2e40a086ab19c045f25a2c27ebbf4c2a6d0427b
Macmilan24 <cpt.macmilan24@gmail.com>
2024-05-07 13:42:57 +0300

    removed intialitems[] since not needed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "charger", quantity: 1, packed: true },
-  { id: 4, description: "bag", quantity: 2, packed: false },
-  { id: 5, description: "shirt", quantity: 5, packed: false },
-  { id: 6, description: "Phone", quantity: 1, packed: false },
-];
-
 export default function App() {
   const [items, setItems] = useState([]);
 

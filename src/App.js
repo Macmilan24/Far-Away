90e3f54322743709cec3eea244f1166921024a4c
Macmilan24 <cpt.macmilan24@gmail.com>
2024-05-07 14:40:21 +0300

    message if there is no items

--- src/App.js
+++ src/App.js
@@ -93,9 +93,17 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
   );
 }
 function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list🚀</em>
+      </p>
+    );
+
   const numItems = items.length;
   const numPacked = items.filter((item) => item.packed).length;
   const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
       <em>
